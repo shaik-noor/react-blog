f02912bd607206e129f8cfa03d5b798c8a26ee81
Shaik_Noor <s.noorink@gmail.com>
2023-08-19 15:00:44 +0530

    adding pages and setting routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticleListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/Article';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>My React App</h1>
-      <p>Welcom To My React App</p>
+      <div id="page-body">
+        <Routes>
+      <Route path="/"element = {<HomePage />} />
+      <Route path="/about" element= {<AboutPage />}/>
+      <Route path="/articles" element = {<ArticleListPage />} />
+      <Route path="/article/:articleid" element = {<ArticlePage />} />
+      </Routes> 
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+const ArticleListPage = () => {
+    return(
+        <h1>This is the article List page</h1>
+    );
+}
+
+export default ArticleListPage;
